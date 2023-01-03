c7061483dc81b4875fe06e979587149f46280fed
Igor Ryazancev <aliengr0001@gmail.com>
2023-01-03 13:57:21 +0800

    Initial Commit

---
+++ film24/film24App.swift
@@ -0,0 +1,17 @@
+//
+//  film24App.swift
+//  film24
+//
+//  Created by Igor Ryazancev on 03.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct film24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
